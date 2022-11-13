f179b4d3bc5f54afdf36bc20617d65428f6a7996
Dor Edelman <doredelman@gmail.com>
2022-11-13 12:03:41 +0200

    User file update with no time and date

--- Assignment1/User.cpp
+++ Assignment1/User.cpp
@@ -4,44 +4,51 @@ User::User(char* tryName, tm tryDate)         //constructor
 {
 	SetName(tryName);
 	//set time , *strTime
-	numberOfFreinds = 0;
+	numberOfFriends = 0;
 	numberOfStatus = 0;		
-	PhisNumberOfFreinds = 10;
-	PhisnumberOfStatus = 10;
-	statusPtrArr = new Status*[PhisnumberOfStatus];
-	friendsPtrArr = new User*[PhisNumberOfFreinds];
+	PhisNumberOfFreinds = initNumberOfFriendsStatus;
+	PhisnumberOfStatus = initNumberOfFriendsStatus;
+	statusPtrArr = new const Status*[PhisnumberOfStatus];
+	friendsPtrArr = new const User*[PhisNumberOfFreinds];
 }
 void User::AddFriend(const User& addFriend)
 {
-	if (numberOfFreinds >= PhisNumberOfFreinds)
-	{
-		PhisNumberOfFreinds *= 2;
-		const User** newFriendsPtrArr = new User *[PhisNumberOfFreinds];
-		for (int i = 0;  i < numberOfFreinds;  i++)
-		{
-			newFriendsPtrArr[i] = friendsPtrArr[i];
-		}
-		delete []friendsPtrArr;
-		friendsPtrArr = newFriendsPtrArr;
-	}
-	friendsPtrArr[numberOfFreinds] = &addFriend;
-	numberOfFreinds++;
-}
-void User::AddStatus(Status& status)    const
-{
+	if (numberOfFriends >= PhisNumberOfFreinds)
+		MakeDoubleFriendsSpace();
 
+	friendsPtrArr[numberOfFriends] = &addFriend;
+	numberOfFriends++;
+}
+void User::AddStatus(const Status& status)
+{
+	if (numberOfStatus >= PhisnumberOfStatus)
+		MakeDoubleStatusSpace();
+
+	statusPtrArr[numberOfStatus] = &status;
+	numberOfStatus++;
 }
 void User::ShowAllStatus()			  const
 {
-
+	for (int i = 0; i < numberOfStatus; i++)
+		statusPtrArr[i]->ShowText();
 }
 void User::ShowAllFriend()			  const
 {
-
+	for (int i = 0; i < numberOfFriends; i++)
+		cout << "Friend number " << (i + 1) << " is: " << friendsPtrArr[i]->name << endl;
 }
-void User::Unfriend(User& freind)	      const
+void User::Unfriend(const User& friendToRemove)
 {
-
+	for (int i = 0; i < numberOfFriends; i++)
+	{
+		if (friendsPtrArr[i] != nullptr && !strcmp(friendsPtrArr[i]->name, friendToRemove.name))
+		{
+			delete friendsPtrArr[i];
+			friendsPtrArr[i] = friendsPtrArr[numberOfFriends - 1];
+			numberOfFriends--;
+			break;
+		}
+	}
 }
 
 bool User::SetName(const char* tryName)
@@ -50,13 +57,40 @@ bool User::SetName(const char* tryName)
 	strcpy(name, tryName);
 	return true;
 }
-
-bool User::SetDateOfBirth(tm& tryDate) const
+bool User::SetDateOfBirth(const tm& tryDate)
 {
+	return true;              //implementation needed.
+}
 
+void User::MakeDoubleFriendsSpace()
+{
+	PhisNumberOfFreinds *= 2;
+	const User** newFriendsPtrArr = new const User * [PhisNumberOfFreinds];
+	for (int i = 0; i < numberOfFriends; i++)
+	{
+		newFriendsPtrArr[i] = friendsPtrArr[i];
+	}
+	delete[]friendsPtrArr;
+	friendsPtrArr = newFriendsPtrArr;
+}
+void User::MakeDoubleStatusSpace()
+{
+	PhisnumberOfStatus *= 2;
+	const Status** newStatusPtrArr = new const Status * [PhisnumberOfStatus];
+	for (int i = 0; i < numberOfStatus; i++)
+	{
+		newStatusPtrArr[i] = statusPtrArr[i];
+	}
+	delete[]statusPtrArr;
+	statusPtrArr = newStatusPtrArr;
 }
 
 User::~User()		//Destructor
 {
-
+	for (int i = 0; i < numberOfStatus; i++)
+	{
+		delete statusPtrArr[i];
+	}
+	delete []friendsPtrArr;                   //dont delete friends address ?
+	delete []statusPtrArr;
 }
--- Assignment1/User.h
+++ Assignment1/User.h
@@ -2,31 +2,33 @@
 #define __User_H
 
 #include "Status.h"
-#include "Date.h"
 #include <ctime> 
+
+const int initNumberOfFriendsStatus = 10;
 
 class User
 {
 public:
 	User(char* tryName, tm tryDate);         //constructor
 	void AddFriend(const User& addFriend);
-	void AddStatus(Status& status)		 const;
-	void ShowAllStatus()				 const;
-	void ShowAllFriend()				 const;
-	void Unfriend(User& freind)			 const;
-
-	bool SetName(const char* tryName);
-	bool SetDateOfBirth(tm& tryDate) const;
-
-
-	~User();									//Destructor
+	void AddStatus(const Status& status);
+	void ShowAllStatus()						const;
+	void ShowAllFriend()					    const;
+	void Unfriend(const User& friendToRemove);
+	~User();								//Destructor
 private:
-	int numberOfFreinds, numberOfStatus;
+	int numberOfFriends, numberOfStatus;
 	int PhisNumberOfFreinds, PhisnumberOfStatus;
 	time_t time, dateOfBirth;
 	char* strTime, *name;
-	Status** statusPtrArr;
+	const Status** statusPtrArr;
 	const User** friendsPtrArr;     //to linked list ?
+
+	bool SetName(const char* tryName);
+	bool SetDateOfBirth(const tm& tryDate);
+
+	void MakeDoubleFriendsSpace();
+	void MakeDoubleStatusSpace();
 };
 
 
